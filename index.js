d248979803c052bf26fb23af9a51f0e494d034fd
Mkmuneeb34734859 <154329450+Mkmuneeb34734859@users.noreply.github.com>
2024-02-29 23:15:14 +0500

    document 34

---
+++ index.js
@@ -0,0 +1,36 @@
+
+//post increment
+var num = 2
+var neWnum = num++
+//jahan ham isko call karenge likhenge to purani value assign karega phir plus karega
+console.log('num->',num)
+console.log('neWnum->',neWnum)
+document.write
+
+//pre increment
+var pre = 2
+var prEnum = ++num
+
+console.log('prenum->',pre)
+console.log('prEnum->',prEnum)
+
+
+
+
+//post decrement
+var pd_num = 2
+var neWpd_num = pd_num--
+
+console.log('pd_num',pd_num)
+console.log('pd_num->',neWpd_num)
+
+
+//pre decrement
+
+var pre = 2
+var prenum = --pre
+
+console.log('prenum->',pre)
+console.log('prenum->',prenum)
+
+
